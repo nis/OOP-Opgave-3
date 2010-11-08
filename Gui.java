919511b409f4228d946398bca4c2354e912bd834
Nis Sarup <nis@sarup.dk>
2010-11-08 18:39:21 +0100

    Added Textarea and List-view.

--- Gui.java
+++ Gui.java
@@ -2,6 +2,10 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Gui extends JFrame {
+	
+	private JList list;
+    private DefaultListModel listModel;
+	
 	public static void main (String args[]) {
 		new Gui();
 	}
@@ -9,13 +13,13 @@ public class Gui extends JFrame {
 	public Gui () {
 		setTitle("OOP Opgave 3");
 	    setDefaultCloseOperation(EXIT_ON_CLOSE);
-	    setSize(500,250);
+	    setSize(545,165);
+		setLocation(650,250);
 	    setVisible(true);
 	
 		Container container = getContentPane();
 		container.setLayout(null); 
 		
-		//JPanel panel = new JPanel(new BorderLayout());
 		JButton addBtn = new JButton("Tilføj");
 	    JButton sortBtn = new JButton("Sortér");
 		JTextField textField = new JTextField();
@@ -23,28 +27,43 @@ public class Gui extends JFrame {
 		JRadioButton aRBtn = new JRadioButton("Arial");
 		JRadioButton sRBtn = new JRadioButton("Serif");
 		
-		
-		//”Courier”, ”Arial” og ”Serif”
+		ButtonGroup rGroup = new ButtonGroup();
+		rGroup.add(cRBtn);
+		rGroup.add(aRBtn);
+		rGroup.add(sRBtn);
+	
+		listModel = new DefaultListModel();
+		list = new JList(listModel);
+        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        list.setSelectedIndex(0);
+	
+		JTextArea tArea = new JTextArea();
 	
 		container.add(addBtn);
-		addBtn.setBounds(20,40,75,25);
+		addBtn.setBounds(100,40,75,25);
 		
 		container.add(sortBtn);
-		sortBtn.setBounds(100,40,75,25);
+		sortBtn.setBounds(100,70,75,25);
 		
 		container.add(textField);
 		textField.setBounds(20,10,155,25);
 		
 		container.add(cRBtn);
 		cRBtn.setSelected(true);
-		cRBtn.setBounds(20,65,155,25);
+		cRBtn.setBounds(20,35,155,25);
 		
 		container.add(aRBtn);
-		aRBtn.setBounds(20,85,155,25);
+		aRBtn.setBounds(20,55,155,25);
 		
 		container.add(sRBtn);
-		sRBtn.setBounds(20,105,155,25);
+		sRBtn.setBounds(20,75,155,25);
+
+		JScrollPane listScrollPane = new JScrollPane(list);
+		container.add(listScrollPane);
+		listScrollPane.setBounds(200,10,155,120);
 		
-		//addBtn.setSize(new Point(200,20));
+		JScrollPane tAreaScrollPane = new JScrollPane(tArea);
+		container.add(tAreaScrollPane);
+		tAreaScrollPane.setBounds(375,10,155,120);
 	}
 }
